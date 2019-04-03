373a04ad4644805ea3f38d6c8a6b4ce085d87b2d
Giampaolo TUCCI <g.tucci@trilogik.it>
2019-04-03 21:45:51 +0200

    primo rilascio

---
+++ UnitTest/Ex01/BizMathLib/BizMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizMathLib
+{
+    public class BizMath
+    {
+        public decimal ApplicaIva(decimal Imponibile, decimal PercIva)
+        {
+
+            return ((Imponibile / 100) * PercIva) + Imponibile;
+        }
+
+        public decimal ScorporaIva(decimal Totale, decimal PercIva)
+        {
+            decimal BaseImponibile = Totale * (100 / (100 + PercIva));
+            BaseImponibile = Math.Round(BaseImponibile,2);
+            return Totale-BaseImponibile;
+        }
+
+
+    }
+}
---
+++ UnitTest/Ex02/NewsLetterLib/LocalDbContext.cs
@@ -0,0 +1,35 @@
+﻿using BizMathLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizMathLib
+{
+    public partial class LocalDbContext : DbContext
+    {
+        public LocalDbContext()
+            : base()
+        {
+
+            
+        }
+
+        public virtual DbSet<ListaMailNewsletter> SaldiFinali { get; set; }
+        
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            Database.SetInitializer<LocalDbContext>(null);
+
+            
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
